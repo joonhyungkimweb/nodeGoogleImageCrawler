2ac2de1a8aa231873bdd37985fcc40e2993fb9de
nick <asy8000@naver.com>
2021-09-30 16:12:14 +0900

    [feat] 서버 설정

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const scraper = require("images-scraper");
+const http = require('http');
+const scraper = require('images-scraper');
+const { URL, URLSearchParams } = require('url');
 
 const google = new scraper({
     puppeteer: {
@@ -6,15 +8,27 @@ const google = new scraper({
     },
 });
 
-console.log("google", google);
-
-(async() => {
+const app = http.createServer(async (req, res) => {
     try {
-        console.log('brower ready');
-        const results = await google.scrape('banana', 200);
-        console.log('results', results);
+        const { search } = new URL(req.url, 'http://localhost:3000');
+
+        const query = new URLSearchParams(search);
+
+        const { keyword, amount } = Array.from(query).reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
+
+        if(keyword == null) throw new Error('network error');
+
+        const results = await google.scrape(keyword, amount);
+
+        res.writeHead(200);
+        res.end(JSON.stringify(results));
     }
     catch (err) {
         console.error(err);
+        res.writeHead(500);
+        res.end();
     }
-})();
+
+})
+
+app.listen(3000);
